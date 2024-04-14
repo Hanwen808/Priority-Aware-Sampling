9f05d49ec18baf8d4ebfd4f6f33e28f3fa6f10a0
Hanwen808 <164186121+Hanwen808@users.noreply.github.com>
2024-04-14 23:01:16 +0800

    Add files via upload

--- C++/Headers/pas.h
+++ C++/Headers/pas.h
@@ -1,3 +1,7 @@
+//
+// Created by lenovo on 2024/1/27.
+//
+
 #ifndef PAS_PAS_H
 #define PAS_PAS_H
 #include "register.h"
@@ -27,11 +31,11 @@ private:
     registerArray R;
     uint32_t C[MAX_PRIORITY];
     float P[MAX_PRIORITY], V[MAX_PRIORITY];
-    float pre;
+    float pre[MAX_PRIORITY];
     uint32_t hash_seed, sample_seed, pre_sample_seed;
     std::unordered_map<uint32_t, std::unordered_map<char*, uint32_t, HashFunc, Cmp>> T;
 public:
-    PAS(uint32_t, float*, float );
+    PAS(uint32_t, float*, float*);
     ~PAS();
     void update(char *,char *, uint32_t);
     uint32_t estimate(char *);
