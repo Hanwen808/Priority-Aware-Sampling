5fdd4b6858d9373b473e0bb76586f05eadfa0108
Hanwen808 <164186121+Hanwen808@users.noreply.github.com>
2024-07-29 14:44:53 +0800

    Update exp_accuracy.sh

--- python/scripts/exp_accuracy.sh
+++ python/scripts/exp_accuracy.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-
-python ../main.py --method $1 --dataset $2 --prior_num $3 --p $4 --mem_kb $5
+echo "nds is running..."
+./exp_nds.sh > ./log_nds.txt
+echo "vhll is running..."
+./exp_vhll.sh > ./log_vhll.txt
+echo "rskt is running..."
+./exp_rSkt.sh > ./log_rskt.txt
+echo "pas is running..."
+./exp_pas.sh > ./log_pas.txt
